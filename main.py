8df474f653e1600c2ac390cd1efd4c31d3a5ce7e
bangbang125 <p178001@asahikawa-kosen.ac.jp>
2018-12-11 03:16:43 +0900

    入力の追加

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ if int(membernum) > 11 :
     print("2チームでやった方がいい")
 
 teamnum = 0
+list = []
 
-while teamnum <= int(membernum) :
+while teamnum < int(membernum) :
     print("誰が参加しますか？ {0} 人中 {1} 人選択済み ".format(membernum,teamnum))
     print("団メンバーコード")
-    print("1.ヨニキ　2.YUUKI  3.あら　4.あてなん　5.ぴす　6.板　7.小人 8.コケバナナ 9.だっちゅ 10.せろりぃ 11.lex3 12.あとおじ　13.れんでぃ")
-    selectmember = input()
-    teamnum + 1
+    print("1.ヨニキ　2.にっしー  3.あら　4.あてなん　5.ぴす　6.板　7.小人 8.コケバナナ 9.だっちゅ 10.せろりぃ 11.lex3 12.あとおじ　13.れんでぃ")
+    i = 0
+    while int(membernum) > i :
+        selectmember = input()
+        list.append(int(selectmember))
+        i+=1
+    i = 0
+    buttle = []
+    while int(membernum) > i :
+        buttle = list
+
+
+
+
+
+
+
 
 
