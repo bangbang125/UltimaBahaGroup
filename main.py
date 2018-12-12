bdea8bcc00b4ebf51389e946fdc5d9af1c4069ea
bangbang125 <p178001@asahikawa-kosen.ac.jp>
2018-12-12 13:04:43 +0900

    一応の動作

--- main.py
+++ main.py
@@ -7,20 +7,30 @@ if int(membernum) > 11 :
 
 teamnum = 0
 list = []
+print("誰が参加しますか？　(名前入力したらEnterを忘れずに)")
+"""print("団メンバーコード")"""\
+"""print("1.ヨニキ　2.にっしー  3.あら　4.あてなん　5.ぴす　6.板　7.小人 8.コケバナナ 9.だっちゅ 10.せろりぃ 11.lex3 12.あとおじ　13.れんでぃ")"""
+i = 0
+while int(membernum) > i :
+    selectmember = input()
+    list.append(selectmember)
+    i+=1
+j = 0
+k = 0
+buttle = list
+"""print(buttle)"""
+while int(membernum) > j:
+    print("{0}{1}{2}{3}{4}{5}".format(buttle[j],
+                                           buttle[j+1 if j+1 < int(membernum)  else j-(int(membernum)-1)],
+                                           buttle[j+2 if j+2 < int(membernum)  else j-(int(membernum)-2)],
+                                           buttle[j+3 if j+3 < int(membernum)  else j-(int(membernum)-3)],
+                                           buttle[j+4 if j+4 < int(membernum)  else j-(int(membernum)-4)],
+                                           buttle[j+5 if j+5 < int(membernum)  else j-(int(membernum)-5)])
+    )
 
-while teamnum < int(membernum) :
-    print("誰が参加しますか？ {0} 人中 {1} 人選択済み ".format(membernum,teamnum))
-    print("団メンバーコード")
-    print("1.ヨニキ　2.にっしー  3.あら　4.あてなん　5.ぴす　6.板　7.小人 8.コケバナナ 9.だっちゅ 10.せろりぃ 11.lex3 12.あとおじ　13.れんでぃ")
-    i = 0
-    while int(membernum) > i :
-        selectmember = input()
-        list.append(int(selectmember))
-        i+=1
-    i = 0
-    buttle = []
-    while int(membernum) > i :
-        buttle = list
+    j+=1
+    k+=1
+
 
 
 
